d22e101466c759d796a508aebf813be443ce0035
Ricardo Rodríguez Lorenzo <ricardo@mhp.es>
2018-06-12 09:21:28 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import DefineMap from 'can-define/map/';
+import route from 'can-route';
+import 'can-route-pushstate';
+import 'can-debug#?./is-dev';
+
+const AppViewModel = DefineMap.extend({
+	page: 'string',
+	slug: 'string',
+	action: 'string',
+
+	title: {
+		default: 'place-my-order',
+		serialize: false
+	}
+});
+
+route.register('{page}', { page: 'home' });
+route.register('{page}/{slug}', { slug: null });
+route.register('{page}/{slug}/{action}', { slug: null, action: null });
+
+export default AppViewModel;
---
+++ src/restaurant/list/list.js
@@ -0,0 +1,53 @@
+import Component from 'can-component';
+import DefineMap from 'can-define/map/';
+import './list.less';
+import view from './list.stache';
+import Restaurant from '~/models/restaurant';
+import State from '~/models/state';
+import City from '~/models/city';
+
+export const ViewModel = DefineMap.extend({
+	get states() {
+		return State.getList({});
+	},
+	state: {
+		type: 'string',
+		default: null,
+		set() {
+			// Remove the city when the state changes
+			this.city = null;
+		}
+	},
+	get cities() {
+		let state = this.state;
+
+		if (!state) {
+			return null;
+		}
+
+		return City.getList({ state });
+	},
+	city: {
+		type: 'string',
+		default: null
+	},
+	get restaurants() {
+		let state = this.state;
+		let city = this.city;
+
+		if (state && city) {
+			return Restaurant.getList({
+				'address.state': state,
+				'address.city': city
+			});
+		}
+
+		return null;
+	}
+});
+
+export default Component.extend({
+	tag: 'pmo-restaurant-list',
+	ViewModel,
+	view
+});
